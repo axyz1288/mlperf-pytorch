6228edb0fdc61e9fda636abac03c8b11a5c7b66e
Eric <axyz1350@gmail.com>
2022-01-13 10:02:16 +0800

    Pretrained MobileNetV1 changed to pytorchcv

--- mobilenetv1.py
+++ mobilenetv1.py
@@ -1,3 +1,4 @@
+from pytorchcv.models.mobilenet import mobilenet_w1
 from torch import nn, Tensor
 from typing import OrderedDict
 
@@ -12,49 +13,7 @@ class MobileNetV1(nn.Module):
 
     def __init__(self):
         super(MobileNetV1, self).__init__()
-
-        def conv_bn(inp, oup, stride):
-            return nn.Sequential(
-                nn.ZeroPad2d((0, 1, 0, 1)),
-                nn.Conv2d(inp, oup, 3, stride, 0, bias=False),
-                nn.BatchNorm2d(oup, eps=0.001),
-                nn.ReLU6()
-            )
-
-        def conv_dw(inp, oup, stride):
-            return nn.Sequential(
-                nn.ZeroPad2d((0, stride - 1, 0, stride - 1)),
-                nn.Conv2d(inp, inp, 3, stride, 1 if stride ==
-                          1 else 0, groups=inp, bias=False),
-                nn.BatchNorm2d(inp, eps=0.001),
-                nn.ReLU6(),
-
-                nn.Conv2d(inp, oup, 1, 1, 0, bias=False),
-                nn.BatchNorm2d(oup, eps=0.001),
-                nn.ReLU6(),
-            )
-
-        self.model = nn.Sequential(
-            conv_bn(3, 32, 2),
-            conv_dw(32, 64, 1),
-            conv_dw(64, 128, 2),
-            conv_dw(128, 128, 1),
-            conv_dw(128, 256, 2),
-            conv_dw(256, 256, 1),
-            conv_dw(256, 512, 2),
-            conv_dw(512, 512, 1),
-            conv_dw(512, 512, 1),
-            conv_dw(512, 512, 1),
-            conv_dw(512, 512, 1),
-            conv_dw(512, 512, 1),
-            conv_dw(512, 1024, 2),
-            conv_dw(1024, 1024, 1),
-            nn.AdaptiveAvgPool2d(1),
-        )
-        self.fc = nn.Linear(1024, 1001)
-
-    def load_state_dict(self, state_dict: OrderedDict[str, Tensor], strict: bool = True):
-        super().load_state_dict(state_dict['state_dict'], strict=strict)
+        self.model = mobilenet_w1(pretrained=True, root='./model/mlperf/pretrained')
 
     def forward(self, x):
         x = self.model(x)
